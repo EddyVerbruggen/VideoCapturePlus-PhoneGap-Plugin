b4b566cb968ad6a30785bd42048f9c1b9fe7e8a7
EddyVerbruggen <eddyverbruggen@gmail.com>
2014-01-05 15:00:38 +0100

    - Android impl - JS interface

--- www/VideoCapturePlus.js
+++ www/VideoCapturePlus.js
@@ -1,9 +1,37 @@
 function VideoCapturePlus() {
 }
 
-// TODO fix params (see -Dev project)
-VideoCapturePlus.prototype.captureVideo = function (message, subject, image, url, successCallback, errorCallback) {
-  cordova.exec(successCallback, errorCallback, "VideoCapturePlus", "captureVideo", [message, subject, image, url]);
+VideoCapturePlus.prototype.captureVideo = function (successCallback, errorCallback, options) {
+  var win = function(pluginResult) {
+    var mediaFiles = [];
+    var i;
+    for (i = 0; i < pluginResult.length; i++) {
+      mediaFiles.push(new MediaFile(
+          pluginResult[i].name,
+          pluginResult[i].fullPath,
+          pluginResult[i].type,
+          pluginResult[i].lastModifiedDate,
+          pluginResult[i].size));
+    }
+    successCallback(mediaFiles);
+  };
+  cordova.exec(win, errorCallback, "VideoCapturePlus", "captureVideo", [options]);
+};
+
+var MediaFile = function(name, fullPath, type, lastModifiedDate, size) {
+  this.name = name;
+  this.fullPath = fullPath;
+  this.type = type;
+  this.lastModifiedDate = lastModifiedDate;
+  this.size = size;
+};
+
+MediaFile.prototype.getFormatData = function(successCallback, errorCallback) {
+  if (typeof this.fullPath === "undefined" || this.fullPath === null) {
+    errorCallback("invalid argument");
+  } else {
+    cordova.exec(successCallback, errorCallback, "VideoCapturePlus", "getFormatData", [this.fullPath, this.type]);
+  }
 };
 
 VideoCapturePlus.install = function () {
