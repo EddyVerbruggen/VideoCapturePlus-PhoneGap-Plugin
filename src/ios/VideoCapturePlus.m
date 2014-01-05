cb537106e6dd67112218065a4669d0baac0ad139
EddyVerbruggen <eddyverbruggen@gmail.com>
2014-01-05 17:18:53 +0100

    Fixed some references

--- src/ios/VideoCapturePlus.m
+++ src/ios/VideoCapturePlus.m
@@ -1,23 +1,4 @@
-/*
- Licensed to the Apache Software Foundation (ASF) under one
- or more contributor license agreements.  See the NOTICE file
- distributed with this work for additional information
- regarding copyright ownership.  The ASF licenses this file
- to you under the Apache License, Version 2.0 (the
- "License"); you may not use this file except in compliance
- with the License.  You may obtain a copy of the License at
- 
- http://www.apache.org/licenses/LICENSE-2.0
- 
- Unless required by applicable law or agreed to in writing,
- software distributed under the License is distributed on an
- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- KIND, either express or implied.  See the License for the
- specific language governing permissions and limitations
- under the License.
- */
-
-#import "CDVCapture.h"
+#import "VideoCapturePlus.h"
 #import <UIKit/UIDevice.h>
 
 // see https://github.com/ednasgoldfishuk/cordova-plugin-media-capture/blob/master/src/ios/CDVCapture.h for full custom overlay
@@ -56,12 +37,12 @@
 
 @end
 
-@implementation CDVCapture
+@implementation VideoCapturePlus
 @synthesize inUse, timer;
 
 - (id)initWithWebView:(UIWebView*)theWebView
 {
-    self = (CDVCapture*)[super initWithWebView:theWebView];
+    self = (VideoCapturePlus*)[super initWithWebView:theWebView];
     if (self) {
         self.inUse = NO;
     }
